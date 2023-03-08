c072d70f7df5fe4118b86acd819e151d9969faaf
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-03-08 15:50:07 +0000

    Added code

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 ZoiaB All rights reserved
+//
+// Created by: ZoiaB
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function doMathClicked () {
+  // this function does basic math
+  document.getElementById('add-math').innerHTML = "1 + 1 + 2 + 2 = " + (1 + 1 + 2 + 2)
+  document.getElementById('multiply-math').innerHTML = "1 * 2 = " + (1 * 2)
+}
